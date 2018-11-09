1cb742b0d9ded9f2c02c867ba1ba885cbaf89117
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-11-09 15:49:22 -0600

    Initial checkin

---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo yum clean all
+sudo rm -rf /var/cache/yum
+sudo rm /var/log/audit/audit.log
---
+++ scripts/rpcbind.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl stop rpcbind
+sudo systemctl disable rpcbind
